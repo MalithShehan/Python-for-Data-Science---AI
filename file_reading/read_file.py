83101593c42d6897f840b650ab07b2fad81af8bd
MalithShehan <shehansandakalum2003@gmail.com>
2024-12-12 10:40:40 +0530

    Create a program that reads the content of a file and display it on the console

--- file_reading/read_file.py
+++ file_reading/read_file.py
@@ -33,6 +33,47 @@
 #     file.write("This is owesome\n")
 #     file.write("Programming is fun")
 
-# Wirtelines method
-with open("file_reading/my_file.txt", "w") as file:
-    file.writelines(["This is owesome\n", "Programming is fun\n", "Python is the best\n"])
+# # Wirtelines method
+# with open("file_reading/my_file.txt", "w") as file:
+#     file.writelines(["This is owesome\n", "Programming is fun\n", "Python is the best\n"])
+
+# Exercise 01. Create a program that reads the content of a file and display it on the console
+# Create a program that storage and manage contacts in a file name contacts.txt. 
+# 01. Each contact should have a name, a phone number and email. 
+# 02. Feature to implement view all contact read and display all contacts on the file. 
+# 03. Add the new contact: apend to the contact file. 
+# 04. Exite on the program.
+# John Doe, 12345678, john@example.com
+# Kate Fery, 87654321, kate@example.com
+
+def add_contact(name, phone, email):
+    with open("file_reading/contacts.txt", "a") as file:
+        file.write(f"{name}, {phone}, {email}\n")
+        print(f"Contact {name} added successfully")
+
+def view_contacts():
+    with open("file_reading/contacts.txt", "r") as file:
+        contacts = file.readlines()
+        for contact in contacts:
+            print(contact)
+
+def main():
+    while True:
+        print("1. Add contact")
+        print("2. View contacts")
+        print("3. Exit")
+        choice = int(input("Enter your choice: "))
+        if choice == 1:
+            name = input("Enter name: ")
+            phone = input("Enter phone: ")
+            email = input("Enter email: ")
+            add_contact(name, phone, email)
+        elif choice == 2:
+            view_contacts()
+        elif choice == 3:
+            break
+        else:
+            print("Invalid choice")
+
+if __name__ == "__main__":
+    main()
