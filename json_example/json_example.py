027cf3a04b59f0a784f810d4f570a0ef0c3cd675
MalithShehan <shehansandakalum2003@gmail.com>
2024-12-12 11:50:50 +0530

    Read json file data

--- json_example/json_example.py
+++ json_example/json_example.py
@@ -1,4 +1,10 @@
-# Read json file
-with open("json_example/example_1.json", "r") as file:
-    content = file.read()
-    print(content, type(content))
+# Read json file data
+import json
+
+josn_file_path = "json_example/example_1.json"
+
+with open(josn_file_path, "r") as file:
+    data = json.load(file)
+    print(data, type(data))
+
+    
