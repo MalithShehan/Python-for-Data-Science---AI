e3d459ea033b62e374efe9891a48cebf807f7fc4
MalithShehan <shehansandakalum2003@gmail.com>
2024-12-12 12:11:29 +0530

    Creat json file & write json file data

--- json_example/json_example.py
+++ json_example/json_example.py
@@ -1,10 +1,25 @@
 # Read json file data
+# import json
+
+# josn_file_path = "json_example/example_1.json"
+
+# with open(josn_file_path, "r") as file:
+#     data = json.load(file)
+#     print(data, type(data))
+
+# Creat json file & write json file data
 import json
 
-josn_file_path = "json_example/example_1.json"
+data = {
+    "name": "John Doe",
+    "age": 30,
+    "city": "New York"
+}
 
-with open(josn_file_path, "r") as file:
-    data = json.load(file)
-    print(data, type(data))
+json_file_path = "json_example/example_3.json"
 
+with open(json_file_path, "w") as json_file:
+    json_data= json.dumps(data, indent = 4)
+    json_file.write(json_data)
+    print(json_file, type(json_file))
     
