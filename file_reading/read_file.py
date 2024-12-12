430cc2bc06d4a65fbad8c8b1b11b5eb8171d6108
MalithShehan <shehansandakalum2003@gmail.com>
2024-12-12 09:51:42 +0530

    Read file content Read the file line Read the file lines as list Open file for writing. Create a new file if it does not exist or truncate the file if it exists Open file for appending at the end of the file without truncating it. Create a new file if it does not exist

---
+++ file_reading/read_file.py
@@ -0,0 +1,30 @@
+# # Read file content
+# file = open("file_reading/my_file.txt", "r")
+# content = file.read()
+# print(content, type(content))
+
+# # Read the file line
+# file = open("file_reading/my_file.txt", "r")
+# content = file.readline()
+# print(content, type(content))
+
+# # Read the file lines as list
+# file = open("file_reading/my_file.txt", "r")
+# content = file.readlines()
+# print(content, type(content))
+
+# # Open file for writing. Create a new file if it does not exist or truncate the file if it exists
+# file = open("file_reading/my_file.txt", "w")
+# content = file.readlines()
+# print(content, type(content))
+
+# # Open file for appending at the end of the file without truncating it. Create a new file if it does not exist
+# file = open("file_reading/my_files.txt", "a")
+# content = file.readlines()
+# print(content, type(content))
+
+# Exercise 01. 
+# 'with' statement is used to open a file and close it automatically
+with open("file_reading/my_file.txt", "r") as file:
+    content = file.read()
+    print(content, type(content))
